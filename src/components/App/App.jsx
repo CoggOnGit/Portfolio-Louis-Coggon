0e7c71e125953881626b451b2fceb0750c5aac6c
CoggOnGit <coggon.louis@gmail.com>
2023-11-07 13:07:56 +0100

    modif basname

--- src/components/App/App.jsx
+++ src/components/App/App.jsx
@@ -11,7 +11,7 @@ import './App.scss';
 function App() {
 
   return (
-    <BrowserRouter basename="/my-portfolio">
+    <BrowserRouter basename="/Portfolio-Louis-Coggon">
       <header>
         <Nav />
       </header>
