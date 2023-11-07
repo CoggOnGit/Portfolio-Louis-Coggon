07819b44e03a549731a3135d429c6115643ce262
CoggOnGit <coggon.louis@gmail.com>
2023-11-07 12:58:46 +0100

    ajout formulaire TypeForm

--- src/components/Contact/Contact.jsx
+++ src/components/Contact/Contact.jsx
@@ -1,27 +1,12 @@
-import { useState } from 'react';
 import './Contact.scss';
+import { Widget } from '@typeform/embed-react'
 
 function Contact() {
-
-  const [showConfirmation, setShowConfirmation] = useState(false);
-  const handleCheckboxChange = () => {
-    setShowConfirmation(!showConfirmation); 
-  };
 
   return (
     <div className='contact' id='contact'>
       <h2>ME CONTACTER</h2>
-      <form name='contact' method='post' className='contact__form'>
-        <div className='formDiv'>
-        <label for='email'>Email: </label>
-        <input type="email" name='email'/>
-
-        <label for='message'>Message: </label>
-        <textarea name='message'></textarea>
-        </div>
-
-        <button type='submit'>Envoyer</button>
-      </form>
+      <Widget id="rUjo0tLf" className="my-form" />
     </div>
   );
 }
